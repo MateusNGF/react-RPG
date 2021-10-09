e1c062dcc90839eecd2b6265318aba22f2c9093f
Mateus Nicolau <mateus2017nicolau@gmail.com>
2021-10-09 12:33:49 -0300

    Limitação do pLayer

--- frontend/src/hooks/usePerson.ts
+++ frontend/src/hooks/usePerson.ts
@@ -1,5 +1,6 @@
 import { useState, KeyboardEvent } from "react";
 import { TypeSide } from "../types/type-side";
+import { mapSpots } from '../data/mapSpots';
 
 export const usePerson = () => {
     const [posicao, setPosicao] = useState({ x: 2, y: 2 });
@@ -7,14 +8,14 @@ export const usePerson = () => {
 
     const MovieLeft = () => {
         setPosicao((ps) => ({
-            x: ps.x - 1,
+            x: canMove(ps.x - 1, ps.y) ? ps.x - 1 : ps.x,
             y: ps.y,
         }));
         setSide("left")
     };
     const MovieRight = () => {
         setPosicao((ps) => ({
-            x: ps.x + 1,
+            x: canMove(ps.x + 1, ps.y) ? ps.x + 1 : ps.x,
             y: ps.y,
         }));
         setSide("right")
@@ -22,17 +23,26 @@ export const usePerson = () => {
     const MovieUp = () => {
         setPosicao((ps) => ({
             x: ps.x,
-            y: ps.y - 1,
+            y: canMove(ps.x, ps.y - 1) ? ps.y - 1 : ps.y
         }));
         setSide("up")
     };
     const MovieDown = () => {
         setPosicao((ps) => ({
             x: ps.x,
-            y: ps.y + 1,
+            y: canMove(ps.x, ps.y + 1) ? ps.y + 1 : ps.y,
         }));
         setSide("down")
     };
+
+    const canMove = (x: number, y: number) => {
+        console.log(x, y)
+        if (x < 0 || y < 0) return false
+        if (mapSpots[y] !== undefined && mapSpots[x] !== undefined) {
+            if (mapSpots[y][x] === 1) return true
+        }
+        return false
+    }
     return {
         x: posicao.x,
         y: posicao.y,
